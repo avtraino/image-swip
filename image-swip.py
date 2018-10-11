9dd33e997ba32951bed83044ef2aadd2f715f99c
Vito Traino <avtraino@gmail.com>
2018-10-11 18:35:04 -0400

    enabled recursing subdirectories (and various other changes)

--- image-swip.py
+++ image-swip.py
@@ -6,19 +6,19 @@ fmt = "%Y-%m-%d_%H.%M.%S"
 dir_path = os.path.dirname(os.path.realpath(__file__))
 data_path = dir_path + "/Data"
 inbox = data_path + '/Inbox/'
-working = data_path + '/Working/'
-processed = data_path + '/Processed/'
+archive = data_path + '/Archive/'
+problem = data_path + '/Problems/'
 
 
-# moves files from Inbox to Working so they cant be modified while processing
+# moves files from Inbox to Archive so they cant be modified while processing
 def protect(title):
     inc_event  = inbox + title + '/'
-    work_event = working + title
-    if not os.path.exists(work_event):
-        os.mkdir(work_event)
+    archive_event = archive + title
+    if not os.path.exists(archive_event):
+        os.mkdir(archive_event)
     folders = os.listdir(inc_event)
     for d in folders:
-        shutil.move(inc_event+d, work_event)
+        shutil.move(inc_event+d, archive_event)
 
 
 # remove directory if empty
@@ -41,42 +41,50 @@ def get_stamp(photo):
 
 def process(title):
     inc_event = inbox + title + '/'
-    work_event = working + title + '/'
-    processed_event = processed + title + '/'
+    archive_event = archive + title + '/'
 
-    if not os.path.exists(processed_event):
-        os.mkdir(processed_event)
+    if not os.path.exists(archive_event):
+        os.mkdir(archive_event)
 
-    photos = os.listdir(work_event)
-    photos = [ f for f in photos if f.lower().endswith(('.jpg', 'png')) ]
-    
-    for photo in photos:
-        original = work_event + '/' + photo
-        suffix = 'b'
-        try:
-            dt = get_stamp(original)
-            stamp = dt.strftime(fmt)
-            nombre = stamp + '__' + title
-            newname = processed_event + nombre + '.jpg'
-
-            # if there are identical files, append a suffix
-            while os.path.exists(newname):
-                newname = stamp + '_' + suffix + '__' + title
-                newname = processed_event + newname + '.jpg'
-                suffix = chr(ord(suffix)+1)
+    for root, subdir, files in os.walk(archive_event):
+        for photo in files:
+            if photo.lower().endswith(('.jpg', 'jpeg')):
+                oldfile = os.path.join(root,photo)
                 
-            shutil.move(original, newname)
-            print(newname)
-        except Exception as e:
-            print("Error: " + str(photo))
-            print("  ^--Exception: " + str(e))
+                suffix = 'b'
+                try:
+                    dt = get_stamp(oldfile)
+                    stamp = dt.strftime(fmt)
+                    newname = stamp + '__' + title + '.jpg'
+                    newfile = os.path.join(root, newname)
 
-    rmdirs(work_event)
+                    # if there are identical files, append a suffix
+                    while os.path.exists(newfile):
+                        newname = stamp + '_' + suffix + '__' + title + '.jpg'
+                        newfile = os.path.join(root, newname)
+                        suffix = chr(ord(suffix)+1)
+                    os.rename(oldfile, newfile)
+                    print(newfile)
+
+                    
+                except Exception as e:
+                    if not os.path.exists(problem):
+                        os.mkdir(problem)
+                    shutil.move(oldfile, problem)
+                    print("Error: " + str(photo))
+                    print("  ^--Exception: " + str(e))
+            # if file is not a jpeg
+            else:
+                if not os.path.exists(problem):
+                    os.mkdir(problem)
+                shutil.move(os.path.join(root,photo) , os.path.join(problem, photo))
+
+
     rmdirs(inc_event)
 
 
 def main():
-    folders = [data_path, inbox, working, processed]
+    folders = [data_path, inbox, archive]
     for folder in folders:
         if not os.path.exists(folder):
             os.mkdir(folder)
@@ -85,15 +93,26 @@ def main():
     for event in incoming:
         path = inbox + event
         if os.path.isdir(path):
-            print("\n################",event,"################")
+            print("\n################################",event,"################################")
             protect(event)
             process(event)
     
 
 def debug():
-    pass
-                
+    folders = [data_path, inbox, archive]
+    for folder in folders:
+        if not os.path.exists(folder):
+            os.mkdir(folder)
 
+    incoming = os.listdir(inbox)
+    for event in incoming:
+        path = inbox + event
+        if os.path.isdir(path):
+            print("\n################################",event,"################################")
+            for root, subdirs, files in os.walk(path):
+                print(root)
+            # photos = [ f for f in photos if f.lower().endswith(('.jpg', 'png')) ]
+            
 
 if __name__ == "__main__":
     main()
